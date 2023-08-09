c1586a522bba0a5b06d4fe845521140062df20d7
Harsha <harsha01111996@gmail.com>
2023-08-09 15:25:01 -0400

    server update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "localhost",
+    HOST: "18.116.50.68",
     USER: "postgres",
     PASSWORD: "password123",
     DB: "resumes",
--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host: "localhost",
+    host: "18.116.50.68",
     user: "postgres",
     password: "password123",
     database: "resumes",
